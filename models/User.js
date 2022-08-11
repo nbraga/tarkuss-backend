262b8b3248869c559125b64682c46f74a216f50f
nbraga <ncls.braga19@gmail.com>
2022-08-11 13:37:28 -0400

    Enviando projeto para produção

---
+++ models/User.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const userSchema = new Schema(
+  {
+    name: String,
+    email: String,
+    password: String,
+    profileImage: String,
+    genre: String,
+    maritalStatus: String,
+    birthDate: Date,
+    cpf: String,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+User = mongoose.model("User", userSchema);
+
+module.exports = User;
